5d0ee1484b195158ca9b54e0b9f4bdb0cc978437
Peachex <klevolex@gmail.com>
2020-12-06 20:24:34 +0300

    add filler

--- src/main/java/com/epam/airline/model/filler/AirportFiller.java
+++ src/main/java/com/epam/airline/model/filler/AirportFiller.java
@@ -3,7 +3,7 @@ package com.epam.airline.model.filler;
 import com.epam.airline.model.entity.Airport;
 import com.epam.airline.model.reader.AirlineReader;
 
-public class AirportFilling {
+public class AirportFiller {
     public void fillAirportWithAirlines(String path) {
         AirlineReader reader = new AirlineReader();
         Airport.getAirport().addToAirlines(reader.readAirlinesFromFile(path));
